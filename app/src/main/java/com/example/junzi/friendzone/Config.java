374608445e9327e51e1bf631515c4aa9102d0088
Roméo Hakanjin <rhakanjin@Newsco.lan>
2017-02-02 11:34:22 +0100

    Ajout fichier de gestion des differentes requetes

--- app/src/main/java/com/example/junzi/friendzone/Config.java
+++ app/src/main/java/com/example/junzi/friendzone/Config.java
@@ -6,26 +6,25 @@ package com.example.junzi.friendzone;
 
 public class Config {
 
-    //Address of our scripts of the CRUD
-    public static final String URL_ADD="http://192.168.94.1/Android/CRUD/addEmp.php";
-    public static final String URL_GET_ALL = "http://192.168.94.1/Android/CRUD/getAllEmp.php";
-    public static final String URL_GET_EMP = "http://192.168.94.1/Android/CRUD/getEmp.php?id=";
-    public static final String URL_UPDATE_EMP = "http://192.168.94.1/Android/CRUD/updateEmp.php";
-    public static final String URL_DELETE_EMP = "http://192.168.94.1/Android/CRUD/deleteEmp.php?id=";
+    //Address of our scripts of the CR
+    public static final String URL_GET_ALL = "http://192.168.224.102/friendzoneapi/CRUD/api/api.php?fichier=users&action=users_liste";
+    /*public static final String URL_GET_EMP = "http://192.168.224.102/friendzoneapi/CRUD/getEmp.php?id=";
+    public static final String URL_UPDATE_EMP = "http://192.168.224.102/friendzoneapi/CRUD/updateEmp.php";
+    public static final String URL_DELETE_EMP = "http://192.168.224.102/friendzoneapi/CRUD/deleteEmp.php?id=";*/
 
     //Keys that will be used to send the request to php scripts
     public static final String KEY_EMP_ID = "id";
     public static final String KEY_EMP_NAME = "name";
-    public static final String KEY_EMP_DESG = "desg";
-    public static final String KEY_EMP_SAL = "salary";
+    public static final String KEY_EMP_PSEUDO = "pseudo";
+    public static final String KEY_EMP_TELEPHONE = "tel";
 
     //JSON Tags
     public static final String TAG_JSON_ARRAY="result";
     public static final String TAG_ID = "id";
-    public static final String TAG_NAME = "name";
-    public static final String TAG_DESG = "desg";
-    public static final String TAG_SAL = "salary";
+    public static final String TAG_NAME = "nom";
+    public static final String TAG_PSEUDO = "pseudo";
+    public static final String TAG_TELEPHONE = "tel";
 
-    //employee id to pass with intent
-    public static final String EMP_ID = "emp_id";
+    /*//employee id to pass with intent
+    public static final String EMP_ID = "emp_id";*/
 }
