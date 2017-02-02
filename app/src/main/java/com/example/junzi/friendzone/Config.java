6e259a61b0feb912aa3ac1e44751a1d26f834716
Roméo Hakanjin <rhakanjin@Newsco.lan>
2017-02-02 14:27:40 +0100

    Connexion a la bdd, et ajout d un exemple afin de comprendre plus rapidement

--- app/src/main/java/com/example/junzi/friendzone/Config.java
+++ app/src/main/java/com/example/junzi/friendzone/Config.java
@@ -7,10 +7,10 @@ package com.example.junzi.friendzone;
 public class Config {
 
     //Address of our scripts of the CR
-    public static final String URL_GET_ALL = "http://192.168.224.102/friendzoneapi/CRUD/api/api.php?fichier=users&action=users_liste";
-    /*public static final String URL_GET_EMP = "http://192.168.224.102/friendzoneapi/CRUD/getEmp.php?id=";
-    public static final String URL_UPDATE_EMP = "http://192.168.224.102/friendzoneapi/CRUD/updateEmp.php";
-    public static final String URL_DELETE_EMP = "http://192.168.224.102/friendzoneapi/CRUD/deleteEmp.php?id=";*/
+    public static final String URL_GET_ALL = "http://192.168.224.102/friendzoneapi/api/api.php?fichier=users&action=users_liste";
+    /*public static final String URL_GET_EMP = "http://192.168.224.102/friendzoneapi/getEmp.php?id=";
+    public static final String URL_UPDATE_EMP = "http://192.168.224.102/friendzoneapi/updateEmp.php";
+    public static final String URL_DELETE_EMP = "http://192.168.224.102/friendzoneapi/deleteEmp.php?id=";*/
 
     //Keys that will be used to send the request to php scripts
     public static final String KEY_EMP_ID = "id";
@@ -25,6 +25,6 @@ public class Config {
     public static final String TAG_PSEUDO = "pseudo";
     public static final String TAG_TELEPHONE = "tel";
 
-    /*//employee id to pass with intent
-    public static final String EMP_ID = "emp_id";*/
+    //employee id to pass with intent
+    public static final String EMP_ID = "emp_id";
 }
