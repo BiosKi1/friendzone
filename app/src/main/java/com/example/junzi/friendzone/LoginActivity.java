dde70ec9aeaeaaad81f9bfc7391a004fa180f127
romeohakanjin <romeo.hakanjin@hotmail.fr>
2017-02-28 23:59:53 +0100

    integration de julien design

--- app/src/main/java/com/example/junzi/friendzone/LoginActivity.java
+++ app/src/main/java/com/example/junzi/friendzone/LoginActivity.java
@@ -17,6 +17,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.inputmethod.EditorInfo;
 import android.widget.AdapterView;
+import android.view.Window;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
